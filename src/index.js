2896174b193be95e883894fcdbcac2685474e2f4
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-10-16 22:44:49 +0200

    saving all results as json file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+const fs = require("fs");
 const olx = require("./olx");
+
+const FILE_NAME = "offers.json";
 
 (async () => {
   await olx.initialize();
   await olx.getResults();
   let results = olx.returnAllResults();
+
+  fs.writeFile(FILE_NAME, JSON.stringify(results, null, 2), "utf-8", err => {
+    if (err) console.log(err);
+    console.log("File has been saved!");
+  });
 })();
