0fffb50e3fa28b22b1bbc7af150b58e494a27952
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-10-17 19:45:21 +0200

    Added helper fns, validating whole results, titles and price

--- src/helpers.js
+++ src/helpers.js
@@ -8,3 +8,44 @@ function convertPrice(price) {
 
   return Number(rawValue);
 }
+
+//only show single offers (not set or multiple items for one high price)
+function validateTitle(title) {
+  const KEY_FLAGS = [
+    "banknoty",
+    "x",
+    "zestaw",
+    "pakiet",
+    "szt",
+    "szt.",
+    "sztuk",
+    "sztuki",
+    "znaczek"
+  ];
+
+  for (let i = 0; i <= KEY_FLAGS.length - 1; i++) {
+    if (title.toLowerCase().includes(KEY_FLAGS[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function validate(array) {
+  //validate offers => return offers only if they meet conditions
+  let filteredArr = array.filter(
+    //title must not contain any KEY FLAGS
+    //PRICE must not be NaN => OLX has 'Change' (PL: Zamienię) option to put instead of amount
+    offer => validateTitle(offer.title) && convertPrice(offer.price)
+  );
+
+  //convert price on each offer
+  filteredArr.forEach(offer => {
+    offer.price = convertPrice(offer.price);
+  });
+
+  return filteredArr;
+}
+
+module.exports = { validate };
