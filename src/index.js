28428d0a1a16a77613288e8bd4fae7464abf8a13
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-10-17 19:49:43 +0200

    Try and catch for error handling, validating results fn  before saving it as json

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const fs = require("fs");
 const OLX = require("./Olx");
+const { validate } = require("./helpers");
 
 const FILE_NAME = "offers.json";
+const olx = new OLX();
 
 (async () => {
-  const olx = new OLX();
-  await olx.initialize();
-  let results = olx.returnAllResults();
+  try {
+    await olx.initialize();
+    let results = olx.returnAllResults();
 
-  fs.writeFile(FILE_NAME, JSON.stringify(results, null, 2), "utf-8", err => {
-    if (err) console.log(err);
-    console.log("File has been saved!");
-  });
+    results = validate(results);
+
+    fs.writeFile(FILE_NAME, JSON.stringify(results, null, 2), "utf-8", err => {
+      if (err) console.log(err);
+      console.log("File has been saved!");
+    });
+  } catch (err) {
+    console.log(`Something wrong... ${err}`);
+  }
 })();
