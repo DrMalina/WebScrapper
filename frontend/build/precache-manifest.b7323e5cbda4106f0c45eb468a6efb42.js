6958f3ab35c91917e774aad4d2fada2df1d93d7a
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-11-04 15:00:30 +0100

    Created build folder

---
+++ frontend/build/precache-manifest.b7323e5cbda4106f0c45eb468a6efb42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6c2f4827ccbd1cca5b81b0d0adc579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30bfa492575def51ad78",
+    "url": "/static/js/2.9745c60a.chunk.js"
+  },
+  {
+    "revision": "3bb7c8fbc0ceebbc9566",
+    "url": "/static/js/main.f02c8136.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "ef6d2fd95706d094b3413fcfc84a0def",
+    "url": "/static/media/bill19zl.ef6d2fd9.jpg"
+  },
+  {
+    "revision": "77dc4548fb79fccebe1e4007f2d1887c",
+    "url": "/static/media/imgsPlaceholder.77dc4548.jpg"
+  }
+]);
